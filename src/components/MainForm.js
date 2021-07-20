b542c0312007284ea71405d683b6c587209a1ec2
Ameya Raje <85251954+areje-cohere@users.noreply.github.com>
2021-07-20 16:19:28 -0400

    api calls added

--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -1,14 +1,34 @@
 import React, { useState, useEffect } from "react";
 import Header from "./Header";
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import qs from 'qs';
+
+const apiUrl = 'http://localhost:1738';
 
 const MainForm = (props) => {
+    // console.log(props.location.search);
     const [feedback, setFeedback] = useState('');
-    const [questionnare, setQuestionnare] = useState('');
+    const [questionnaire, setQuestionnaire] = useState();
 
+    var qid = qs.parse(props.location.search, { ignoreQueryPrefix: true }).questionnaireID;
+    
     useEffect(() => {
+        if (props.location.search) {
+            // var qid = qs.parse(props.location.search, { ignoreQueryPrefix: true }).questionnaireID;
+            console.log(qid)
+            fetch(`${apiUrl}/patientQuestionnaire/${qid}`, { mode: 'no-cors' })
+                .then(response => response.json())
+                .then((data) => {
+                    setQuestionnaire(data);
+                    // console.log("Success: ", data);
+                })
+                .catch((error) => {
+                    console.error("Error: ", error);
+                });
 
-    }, [questionnare]);
+            setQuestionnaire(qid);
+        }
+    }, [questionnaire, props.location.search, qid]);
 
     const onFeedbackEntered = (e) => {
         const feedbackValue = e.target.value;
@@ -16,8 +36,20 @@ const MainForm = (props) => {
     };
 
     const onSubmit = (e) => {
-        // add fetch call here 
-        props.history.push("/thanks");
+        // make a PUT request here to the DB
+        fetch(`${apiUrl}/`, {
+            method: 'PUT',
+            body: {
+                id: qid,
+                response: feedback
+            }
+        })
+        .then(response => response.json())
+        .then(() => {
+            props.history.push("/thanks");
+        })
+        .error(err => console.log(err))
+
     };
 
     return (
@@ -27,9 +59,10 @@ const MainForm = (props) => {
                 <form onSubmit={onSubmit}>
                     <h1>We'd love to hear from you!</h1>
                     <h4>Please provide feedback about your experience at blah for foo!</h4>
+                    {/* <h4>{questionnaire}</h4> */}
                     <textarea
                         className="form-control"
-                        placeHolder="Enter your feedback here"
+                        placeholder="Enter your feedback here"
                         value={feedback}
                         onChange={onFeedbackEntered}
                     >
